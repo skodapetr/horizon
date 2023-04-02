9c7ff54ad029ae44ce9ab954e5abc38c01b26f5f
Petr Škoda <skodapetr@gmail.com>
2023-04-02 21:37:33 +0200

    Add symlink option
    It allows to symlink last version of the file.

--- data-endpoint/src/create_accessibility_report.py
+++ data-endpoint/src/create_accessibility_report.py
@@ -11,6 +11,7 @@ import argparse
 import typing
 import urllib.request
 import contextlib 
+import pathlib
 
 from SPARQLWrapper import SPARQLWrapper, JSON
 
@@ -45,17 +46,25 @@ def _read_arguments() -> dict[str, str]:
         required=True,
         help="Path to directory where to write output.",
     )
+    parser.add_argument(
+        "--symlink",
+        action="store_true",
+        help="Create symlink to output file.",
+    )
     return vars(parser.parse_args())
 
 
 def main(args):
-    _initialize_logging()
+    initialize_logging()
     endpoints = list_endpoints_from_csv(args["sparql_endpoints"])
     availability = test_endpoint_availability(endpoints)
-    write_report(availability, args["output_directory"])
+    file_name = write_report(availability, args["output_directory"])
+    if args["symlink"]:
+        symlink_report(args["output_directory"], file_name)
 
 
-def _initialize_logging():
+
+def initialize_logging():
     logging.basicConfig(
         format="%(asctime)s [%(levelname)s] %(name)s : %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
@@ -114,7 +123,7 @@ def test_endpoint(url: str) -> bool:
         return False
 
 
-def write_report(endpoints: list[ReportItem], directory: str):
+def write_report(endpoints: list[ReportItem], directory: str) -> str:
     today_as_string = datetime.datetime.today().strftime("%Y-%m-%d")
     report: Report = {
         "metadata": {"date": today_as_string, "timeout": TIMEOUT_SECOND},
@@ -122,13 +131,21 @@ def write_report(endpoints: list[ReportItem], directory: str):
     }
     file_name = f"{today_as_string}-sparql-available.json"
     os.makedirs(directory, exist_ok=True)
-    _write_json(os.path.join(directory, file_name), report)
+    write_json(os.path.join(directory, file_name), report)
+    return file_name
 
 
-def _write_json(path: str, content):
+def write_json(path: str, content):
     with open(path, "w", encoding="utf-8") as stream:
         json.dump(content, stream, ensure_ascii=False, indent=2)
 
+
+def symlink_report(directory:str, file_name: str) -> None:
+    """Update symlink to newly created file."""
+    source = pathlib.Path(directory) / file_name
+    destination = pathlib.Path(directory) / "sparql-available.json"
+    os.remove(destination)
+    source.symlink_to(destination)
 
 if __name__ == "__main__":
     main(_read_arguments())
