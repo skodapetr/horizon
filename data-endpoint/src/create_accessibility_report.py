32c4dfafb67e9a824af3b34d16bc4afce4f36ff6
Petr Škoda <skodapetr@gmail.com>
2023-07-11 17:33:02 +0200

    Update data-endpoint script
    Update output data, consume sparql-endpoints.json, add last online information, #3.

--- data-endpoint/src/create_accessibility_report.py
+++ data-endpoint/src/create_accessibility_report.py
@@ -18,39 +18,21 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 TIMEOUT_SECOND = 5
 
+TIME_FORMAT = "%Y-%m-%d"
 
-class EndpointStatus(enum.Enum):
+STATUS_UNAVAILABLE = "unavailable"
 
-    UNAVAILABLE = "unavailable"
+STATUS_QUERY_FAILED = "invalid"
 
-    INVALID = "invalid"
-
-    AVAILABLE = "available"
-
-
-ReportItem = typing.TypedDict(
-    "ReportItem",
-    {
-        "endpoint": str,
-        "status": EndpointStatus,
-    },
-)
-
-Report = typing.TypedDict(
-    "Report",
-    {
-        "metadata": typing.TypedDict("ReportMetadata", {"date": str, "timeout": int, "version": int}),
-        "data": list[ReportItem],
-    },
-)
+STATUS_AVAILABLE = "available"
 
 
-def _read_arguments() -> dict[str, str]:
+def read_arguments() -> dict[str, str]:
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--sparql-endpoints",
         required=True,
-        help="Path to CSV file, or URL, with SPARQL endpoints.",
+        help="Path or URL to a file with endpoints.",
     )
     parser.add_argument(
         "--output-directory",
@@ -60,18 +42,18 @@ def _read_arguments() -> dict[str, str]:
     parser.add_argument(
         "--symlink",
         action="store_true",
-        help="Create symlink to output file.",
+        help="Create symlink to created report. Use this to have stable file for latest results.",
     )
     return vars(parser.parse_args())
 
 
 def main(args):
     initialize_logging()
-    endpoints = list_endpoints_from_csv(args["sparql_endpoints"])
-    availability = test_endpoint_availability(endpoints)
-    file_name = write_report(availability, args["output_directory"])
+    endpoints = load_endpoints(args["sparql_endpoints"])
+    availability = test_endpoints_availability(endpoints)
+    report_file = write_report(availability, args["output_directory"])
     if args["symlink"]:
-        symlink_report(args["output_directory"], file_name)
+        symlink_report(args["output_directory"], report_file)
 
 
 def initialize_logging():
@@ -82,18 +64,21 @@ def initialize_logging():
     )
 
 
-def list_endpoints_from_csv(path: str):
-    # Ignore duplicit records.
-    visited = set()
+def load_endpoints(path: str):
     with open_stream(path) as stream:
-        reader = csv.reader(stream)
-        next(reader)
-        for row in reader:
-            url = row[1]
-            if url in visited:
-                continue
-            else:
-                yield url
+        content = json.load(stream)
+    base = content["@context"]["@base"]
+    return [
+        {
+            # Resource.
+            "@id": base + item["@id"],
+            # Relative, use to construct report URL.
+            "relative": item["@id"],
+            # SPARQL Endpoint URL.
+            "url": item["url"],
+        }
+        for item in content["endpoint"]
+    ]
 
 
 def open_stream(path: str):
@@ -110,33 +95,25 @@ def url_as_lines(url: str):
         yield lines
 
 
-def test_endpoint_availability(endpoints) -> list[ReportItem]:
-    result: list[ReportItem] = []
-    for url in endpoints:
-        available, is_sparql = test_endpoint(url)
+def test_endpoints_availability(endpoints):
+    result = []
+    for index, endpoint in enumerate(endpoints):
+        url = endpoint["url"]
+        available, can_query = test_endpoint(url)
         if not available:
-            result.append({
-                "endpoint": url,
-                "status": EndpointStatus.UNAVAILABLE,
-            })
-            logging.info("'%s' is unavailable.", url)
-            continue
-        if not is_sparql:
-            result.append({
-                "endpoint": url,
-                "status": EndpointStatus.INVALID,
-            })
-            logging.info("'%s' is not SPARQL endpoint.", url)
-            continue
-        result.append({
-            "endpoint": url,
-            "status": EndpointStatus.AVAILABLE,
-        })
-        logging.info("'%s' is available.", url)
+            status = STATUS_UNAVAILABLE
+        elif not can_query:
+            status = STATUS_QUERY_FAILED
+        else:
+            status = STATUS_AVAILABLE
+        logging.info(f"{index + 1}/{len(endpoints)} '{url}' : {status}")
+        result.append({**endpoint, "status": status})
+        if index > 3:
+            break
     return result
 
 
-def test_endpoint(url: str) -> tuple[bool, bool, bool]:
+def test_endpoint(url: str) -> tuple[bool, bool]:
     """Using a simple ASK query test the endpoint."""
     endpoint = SPARQLWrapper(url)
     endpoint.setTimeout(TIMEOUT_SECOND)
@@ -151,22 +128,86 @@ def test_endpoint(url: str) -> tuple[bool, bool, bool]:
     return True, True
 
 
-def write_report(endpoints: list[ReportItem], directory: str) -> str:
-    today_as_string = datetime.datetime.today().strftime("%Y-%m-%d")
-    report: Report = {
-        "metadata": {"date": today_as_string, "timeout": TIMEOUT_SECOND, "version": 1},
-        "data": [
-            {
-                "endpoint": item["endpoint"],
-                "status": item["status"].value,
-            }
-            for item in endpoints
-        ],
+def write_report(endpoints: list, directory: str) -> str:
+    today = datetime.datetime.today()
+
+    availability_map = {}
+    last_date = None
+    last_report = load_json(os.path.join(directory, "sparql.json"))
+    if last_report is not None:
+        last_date = last_report["metadata"]["created"]
+        logging.info(f"Loaded last report from {last_date}.")
+        availability_map = {
+            report_item["@id"]: report_item for report_item in last_report["report"]
+        }
+
+    report_items = []
+    for endpoint in endpoints:
+        report_item = {
+            "@id": endpoint["relative"],
+            "endpoint": endpoint["@id"],
+            "url": endpoint["url"],
+            "status": endpoint["status"],
+        }
+        # Add information about last time we see in online.
+        if not endpoint["status"] == STATUS_AVAILABLE:
+            last_available = None
+            last_report = availability_map.get(report_item["@id"], None)
+            if last_report is not None:
+                if last_report["status"] == STATUS_AVAILABLE:
+                    last_available = last_date
+                else:
+                    last_available = last_report.get("lastAvailable", None)
+            if last_available is not None:
+                report_item["lastAvailable"] = last_available
+        # Store to item list.
+        report_items.append(report_item)
+
+    report = {
+        "@context": {
+            "@base": "https://skodapetr.github.io/horizon/v1/data/sparql/2023-07-11/",
+            "@vocab": "https://skodapetr.github.io/horizon/v1/schema#",
+            "xsd": "http://www.w3.org/2001/XMLSchema#",
+            "void": "http://rdfs.org/ns/void#",
+            "url": {"@id": "void:sparqlEndpoint", "@type": "@id"},
+            "endpoint": {"@type": "@id"},
+            "lastAvailable": {"@type": "xsd:date"},
+            "status": {
+                "@type": "@id",
+                "@context": {
+                    "@base": "https://skodapetr.github.io/horizon/v1/resource/SparqlStatus/"
+                },
+            },
+            "created": {
+                "@type": "xsd:date",
+                "@id": "dcterms:created",
+            },
+        },
+        "@id": "",
+        "metadata": {
+            "@id": "",
+            "created": today.strftime(TIME_FORMAT),
+            "timeoutSecond": TIMEOUT_SECOND,
+            "version": 1,
+        },
+        "report": report_items,
     }
-    file_name = f"sparql-{today_as_string}.json"
     os.makedirs(directory, exist_ok=True)
-    write_json(os.path.join(directory, file_name), report)
-    return file_name
+    output_path = report_path(directory, today)
+    write_json(output_path, report)
+    return output_path
+
+
+def report_path(directory: str, date: datetime.datetime):
+    file_name = f"sparql-{date.strftime(TIME_FORMAT)}.json"
+    return os.path.join(directory, file_name)
+
+
+def load_json(path: str):
+    if not os.path.exists(path):
+        return None
+    with open(path, encoding="utf-8") as stream:
+        return json.load(stream)
 
 
 def write_json(path: str, content):
@@ -174,13 +215,12 @@ def write_json(path: str, content):
         json.dump(content, stream, ensure_ascii=False, indent=2)
 
 
-def symlink_report(directory: str, file_name: str) -> None:
+def symlink_report(directory: str, target_path: str) -> None:
     """Update symlink to newly created file."""
-    source = pathlib.Path(directory) / file_name
-    destination = pathlib.Path(directory) / "sparql.json"
-    destination.unlink(missing_ok=True)
-    destination.symlink_to(source)
+    symlink = pathlib.Path(directory) / "sparql.json"
+    symlink.unlink(missing_ok=True)
+    symlink.symlink_to(target_path)
 
 
 if __name__ == "__main__":
-    main(_read_arguments())
+    main(read_arguments())
